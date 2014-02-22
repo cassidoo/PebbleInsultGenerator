a57b1db9da7a51d248577d37885e24c1b6ba32ff
Cassidy Williams <cassidoo@outlook.com>
2014-02-21 22:30:57 -0600

    Removing subtitle, changing things to text

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 simply.title('Insult Generator');
+simply.subtitle('');
 simply.scrollable(true);
 
 var initialblow = ['Away I say', 'Bathe thyself', 'Be not deaf', 'Behold thy mirror', 'Beware my sting', 'Clean thine ears', 'Drink up eisel', 'Eat a crododile', 'Eat my knickers', 'Fie upon thee', 'Forsooth say I', 'Get thee gone', 'Get thee hence', 'Grow unsightly warts', 'Hear me now', 'Hear this pox alert', 'I shall see thee hanged', 'Kiss my codpiece', 'Lead apes in hell', 'Methinks you stinks', 'My finger in thine eye', 'Phui, I say', 'Remove thine ass hence', 'Resign not thy day gig', 'Sit thee on a spit', 'Sorrow on thee', 'Swim with leeches', 'Thou dost intrude', 'Thy mother wears armor', 'Trip on thy sword', 'Tune thy lute', 'Why, how now putz', 'Wipe thy ugly face'];
@@ -27,6 +28,6 @@ simply.on('singleClick', function(e) {
   
   localStorage.setItem('previous', insult);*/
   
-  simply.subtitle(insult);
+  simply.text(insult);
   
 });
