df0fbacf938e0db31b38d1641b825ee555f5508b
Cassidy Williams <cassidoo@outlook.com>
2014-03-09 01:57:55 -0600

    Pebble help from @phardy
    #NotACDev

--- main.c
+++ main.c
@@ -23,30 +23,39 @@ char* genInsult()
 }
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "test"); // genInsult();
+	text_layer_set_text(text_layer, "test"); // genInsult();
 }
 
 static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, select_click_handler);
+	window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+	window_single_click_subscribe(BUTTON_ID_UP, select_click_handler);
+	window_single_click_subscribe(BUTTON_ID_DOWN, select_click_handler);
+}
+
+static void window_load(Window *window) {
+	Layer *window_layer = window_get_root_layer(window);
+	GRect bounds = layer_get_bounds(window_layer);
+	
+	text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
+	text_layer_set_text(text_layer, "Press button for insults");
+	
+	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+	layer_add_child(window_layer, text_layer_get_layer(text_layer));
+}
+ 
+static void window_unload(Window *window) {
+	text_layer_destroy(text_layer);
 }
 
 void handle_init(void) {
 	window = window_create();
-
-	text_layer = text_layer_create((GRect){ .origin = { 0, 30 }, .size = bounds.size });
-	
-	window_set_click_config_provider(window, (ClickConfigProvider) config_provider); //&window?	
-	
-	text_layer_set_text(text_layer, genInsult());
-	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-	
-	layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
-
-	window_stack_push(window, true);	
-
+	window_set_click_config_provider(window, click_config_provider);
+	window_set_window_handlers(window, (WindowHandlers) {
+		.load = window_load,
+		.unload = window_unload,
+	});
+	const bool animated = true;
+	window_stack_push(window, animated);	
 }
 
 void handle_deinit(void) {
